591ba26f2aa4cab658988f79b18832fd08d610bb
Raunaq Singh <raunaq.singh2007@gmail.com>
2026-08-15 17:52:20 +0530

    array

---
+++ reversearray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int temp;
+    int arr[5] = {1, 2, 3, 4, 5};
+    for(int i=0; i<5/2; i++){
+        temp = arr[i];
+        arr[i] = arr[5-i-1];
+        arr[5-i-1] = temp;
+    }
+    for(int i=0; i<5; i++){
+        cout << arr[i] << " "; 
+    }
+}
