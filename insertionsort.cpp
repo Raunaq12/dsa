f3ad91aba4c4d9fcb4eef47a5a910581fab594b9
Raunaq Singh <raunaq.singh2007@gmail.com>
2026-08-21 14:01:05 +0530

    insertion

---
+++ insertionsort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[5] = {5, 4, 3, 2, 1};
+    for(int i=0; i<5; i++){
+        int j=i;
+        while(j>0 && arr[j-1] > arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+    for(int i=0; i<5; i++){
+        cout << arr[i] << " ";
+    }
+}
--- selectionsort.cpp
+++ selectionsort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int arr[15] = {42, 17, 89, 3, 56, 91, 24, 73, 11, 68, 35, 82, 5, 49, 27};
     int min_element = arr[0];
-    for(int i=1; i<14; i++){
+    for(int i=0; i<14; i++){
         int min = i;
         for(int j=i+1; j<15; j++){
             if(arr[j] < arr[min]){
