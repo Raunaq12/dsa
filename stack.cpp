c15f003c68353b684936ff7b7acf0482f010e68b
Raunaq Singh <raunaq.singh2007@gmail.com>
2026-09-11 15:42:39 +0530

    stack

---
+++ stack.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+    stack<int> s1;
+    s1.push(1);
+    s1.push(5);
+    cout << s1.top() << endl;
+    s1.pop();
+    cout << s1.top() << endl;
+
+}
